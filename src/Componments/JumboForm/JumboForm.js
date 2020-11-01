a44bf8c8d2cc477149d953031a9926f8c9509bdf
Freddy <1020080424@qq.com>
2020-11-01 15:47:36 +0800

    Finish Jumboform

--- src/Componments/JumboForm/JumboForm.js
+++ src/Componments/JumboForm/JumboForm.js
@@ -1,9 +1,60 @@
 import React from 'react'
-import {Form,Button, Item} from 'semantic-ui-react'
+import { Form, Button, Item } from 'semantic-ui-react'
+import csc from 'country-state-city'
+//const cities = require('all-the-cities');
 class JumboForm extends React.Component {
-    handleClick=(e)=>{
+    constructor(props) {
+        super(props)
+        this.state = {
+            country: {},
+            state: {},
+            city: {},
+        }
+    }
+    handleClick = (e) => {
         const form = e.target.parentElement
-        const formInput = form[0]
+        var formInput = form[2]
+        if (formInput.value==''){
+            formInput=form[1]
+        }
+        console.log(form)
+        console.log(formInput)
+        const p = this.props
+        p.find(formInput.value)
+    }
+    selectedCountry = (e) => {
+        const form = e.target.parentElement
+        const value = form.children[0].value
+        const countries=csc.getAllCountries()
+        const exists = countries.find(item => {
+            return item.name === value
+        })
+        if (!exists) {
+            return -1
+        }
+        this.setState({
+            country: exists
+        })
+    }
+    selectedState = (e) => {
+        const form = e.target.parentElement
+        const value = form.children[1].value
+        const countries=csc.getStatesOfCountry(this.state.country.id)
+        const exists = countries.find(item => {
+            return item.name === value
+        })
+        if (!exists) {
+            return -1
+        }
+        this.setState({
+            state: exists
+        })
+    }
+    selectedCity = (e) => {
+        const form = e.target.parentElement
+        const formInput = form[2]
+        console.log(form)
+        console.log(formInput)
         const p = this.props
         p.find(formInput.value)
     }
@@ -13,13 +64,30 @@ class JumboForm extends React.Component {
                 display: 'flex'
             }}> {/*  make it a flex display  */}
                 <Form.Field style={{
-                    width:'70%',
-                    margin:0
+                    width: '70%',
+                    margin: 0
                 }}> {/*  make it 70% and remove margin  */}
-                    <input id='input' placeholder='City Name' />
+                    <input id='input' list='countries' placeholder='Country Name' onSelect={this.selectedCountry} />
+                    <input id='input' list='states' placeholder='State Name' onSelect={this.selectedState}/>
+                    <input id='input' list='cities' placeholder='City Name'/>
+                    <datalist id='cities'>
+                        {csc.getCitiesOfState(this.state.state.id).map((item) => {
+                            return <option value={item.name}>{item.name}</option>
+                        })}
+                    </datalist>
+                    <datalist id='states'>
+                        {csc.getStatesOfCountry(this.state.country.id).map((item) => {
+                            return <option value={item.name}>{item.name}</option>
+                        })}
+                    </datalist>
+                    <datalist id='countries'>
+                        {csc.getAllCountries().map((item) => {
+                            return <option value={item.name}>{item.name}</option>
+                        })}
+                    </datalist>
                 </Form.Field>
                 <Button onClick={this.handleClick} type='submit'>Submit</Button>
-                
+
             </Form>
         </div>
     }
